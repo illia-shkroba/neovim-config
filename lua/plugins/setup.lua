2f2d19b3f7433fab0bcde48d8afbd56b35b727b8
Illia Shkroba <is@pjwstk.edu.pl>
2023-05-22 12:44:59 +0200

    Allow to specify module's field in plugins.setup module

--- lua/plugins/setup.lua
+++ lua/plugins/setup.lua
@@ -1,7 +1,9 @@
-return function(module_name)
+return function(module_name, field)
+  local field = field or "setup"
+
   local m = require("utils").require_safe(module_name)
-  if type(m) == "table" and m.setup then
-    return m.setup
+  if type(m) == "table" and m[field] then
+    return m[field]
   else
     return function() end
   end
