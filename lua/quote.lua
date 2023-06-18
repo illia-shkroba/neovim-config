c2d997f55f41785712f3af8ed56389a4430d7183
Illia Shkroba <is@pjwstk.edu.pl>
2023-06-18 16:06:22 +0200

    Add support of more motions for quote module

--- lua/quote.lua
+++ lua/quote.lua
@@ -7,21 +7,9 @@ local utils = require "utils"
 
 function M.normal()
   local quote = fn.getcharstr()
-  local result = utils.read_number()
-  local count, text_object = result.number, result.rest
+  local motion = utils.read_motion { allow_forced = false }
 
-  if vim.tbl_contains({ "i", "a" }, text_object) then
-    text_object = text_object .. fn.getcharstr()
-  end
-
-  local string_count
-  if count == 0 then
-    string_count = ""
-  else
-    string_count = tostring(count)
-  end
-
-  cmd.normal("v" .. string_count .. text_object .. M.quote(quote))
+  cmd.normal("v" .. motion.count .. motion.motion .. M.quote(quote))
 end
 
 function M.visual()
