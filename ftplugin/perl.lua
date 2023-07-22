db72b58e326bd6a5795fe513d96eb38041f52e65
Illia Shkroba <is@pjwstk.edu.pl>
2023-07-22 11:57:13 +0200

    Add Perl options

---
+++ ftplugin/perl.lua
@@ -0,0 +1,15 @@
+if vim.b.did_perl_ftplugin then
+  return
+end
+vim.b.did_perl_ftplugin = true
+
+local opt_local = vim.opt_local
+local set = vim.keymap.set
+
+opt_local.expandtab = true
+opt_local.formatprg = "LC_ALL='C' perltidy"
+opt_local.shiftwidth = 4
+opt_local.softtabstop = 4
+opt_local.tabstop = 4
+
+set("", [[<leader><CR>]], [[<Cmd>w !perl<CR>]], { buffer = true })
--- lua/options.lua
+++ lua/options.lua
@@ -333,6 +333,7 @@ function M.enable_templates()
   enable_template "hs"
   enable_template "java"
   enable_template "md"
+  enable_template "pl"
   enable_template "scala"
   enable_template "sh"
 end
