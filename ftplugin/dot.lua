3f073c1d7b5d8fd182490ed2cd2e635c68c78256
Illia Shkroba <is@pjwstk.edu.pl>
2023-08-23 10:26:21 +0200

    Add dot options

---
+++ ftplugin/dot.lua
@@ -0,0 +1,11 @@
+if vim.b.did_dot_ftplugin then
+  return
+end
+vim.b.did_dot_ftplugin = true
+
+local opt_local = vim.opt_local
+
+opt_local.expandtab = true
+opt_local.shiftwidth = 2
+opt_local.softtabstop = 2
+opt_local.tabstop = 2
