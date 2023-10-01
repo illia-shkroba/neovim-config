6181d0a224ac983dba7bfdc785e22734562ccaee
Illia Shkroba <is@pjwstk.edu.pl>
2023-10-01 17:46:21 +0200

    Add dhall options

---
+++ ftplugin/dhall.lua
@@ -0,0 +1,22 @@
+if vim.b.did_dhall_ftplugin then
+  return
+end
+vim.b.did_dhall_ftplugin = true
+
+local opt_local = vim.opt_local
+local set = vim.keymap.set
+
+opt_local.equalprg = "dhall lint"
+opt_local.expandtab = true
+opt_local.formatprg = "dhall format"
+opt_local.shiftwidth = 4
+opt_local.softtabstop = 4
+opt_local.tabstop = 4
+
+set("", [[<leader><CR>]], [[<Cmd>w !dhall-to-json<CR>]], { buffer = true })
+set(
+  "n",
+  [[<leader><Tab>]],
+  [[<Cmd>up<CR>:new<CR>:terminal dhall-to-json --file #<CR>]],
+  { buffer = true }
+)
