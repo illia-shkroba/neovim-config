f4e699ce1547c7389bec103068ef765bad72a9b6
Illia Shkroba <is@pjwstk.edu.pl>
2023-10-01 17:46:21 +0200

    Add dhall options

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -1,5 +1,6 @@
 return {
+  "tpope/vim-fugitive",
   { "LnL7/vim-nix", ft = { "nix" } },
   { "pearofducks/ansible-vim", ft = { "yaml" } },
-  "tpope/vim-fugitive",
+  { "vmchale/dhall-vim", ft = { "dhall" } },
 }
