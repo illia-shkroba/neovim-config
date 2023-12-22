432ab28c85d104597982a28a13ceb9bbd715dd24
Illia Shkroba <is@pjwstk.edu.pl>
2023-12-22 11:45:40 +0100

    Add bindings for Gdiffsplit in a new tab

--- lua/options.lua
+++ lua/options.lua
@@ -260,8 +260,10 @@ function M.set_default_bindings()
   end
 
   -- git
-  set("n", [[<leader>DS]], [[<Cmd>:Gdiffsplit! HEAD<CR>]])
+  set("n", [[<leader>DS]], [[<Cmd>:wincmd s | wincmd T | Gdiffsplit! HEAD<CR>]])
+  set("n", [[<leader>Ds]], [[<Cmd>:Gdiffsplit! HEAD<CR>]])
   set("n", [[<leader>ds]], [[<Cmd>:Gdiffsplit!<CR>]])
+  set("n", [[<leader>dS]], [[<Cmd>:wincmd s | wincmd T | Gdiffsplit!<CR>]])
   set("n", [[<leader>P]], [[<Cmd>:Git add --patch -- %<CR>]])
 
   -- yield
