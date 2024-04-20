4cf5fbaa4938ff9dee4b255bf58afbc4c3557a2d
Illia Shkroba <is@pjwstk.edu.pl>
2024-04-20 14:10:22 +0200

    Add Rust options

---
+++ ftplugin/rust.lua
@@ -0,0 +1,24 @@
+if vim.b.did_rust_ftplugin then
+  return
+end
+vim.b.did_rust_ftplugin = true
+
+local cmd = vim.cmd
+local opt_local = vim.opt_local
+local set = vim.keymap.set
+
+opt_local.colorcolumn = "100"
+opt_local.expandtab = true
+opt_local.formatprg = "rustfmt"
+opt_local.shiftwidth = 4
+opt_local.softtabstop = 4
+opt_local.tabstop = 4
+opt_local.makeprg = "cargo build"
+
+set("n", [[<leader><CR>]], function()
+  cmd.update()
+  cmd.new()
+
+  cmd.terminal "cargo run --quiet"
+  cmd.startinsert()
+end, { buffer = true })
