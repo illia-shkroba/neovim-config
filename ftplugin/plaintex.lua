39fdf5df867b50995ddea731dd5d247d3519d951
Illia Shkroba <is@pjwstk.edu.pl>
2024-07-16 20:22:04 +0200

    Removed unused bindings from markdown, plaintex and text ftplugins

--- ftplugin/plaintex.lua
+++ ftplugin/plaintex.lua
@@ -2,30 +2,3 @@ if vim.b.did_plaintex_ftplugin then
   return
 end
 vim.b.did_plaintex_ftplugin = true
-
-local set = vim.keymap.set
-
-set(
-  "",
-  [[gqap]],
-  [[:'{,'}substitute/\s\+/ /g<CR>gqap]],
-  { buffer = true, remap = false }
-)
-set(
-  "",
-  [[gqip]],
-  [[:'{,'}substitute/\s\+/ /g<CR>gqip]],
-  { buffer = true, remap = false }
-)
-set(
-  "",
-  [[gqq]],
-  [[:substitute/\s\+/ /g<CR>gqq]],
-  { buffer = true, remap = false }
-)
-set(
-  "",
-  [[gww]],
-  [[:substitute/\s\+/ /g<CR>gww]],
-  { buffer = true, remap = false }
-)
