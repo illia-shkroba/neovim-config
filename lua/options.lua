be99733a4f1a89ee25d7aaafa5f8732756d682d2
Illia Shkroba <is@pjwstk.edu.pl>
2024-08-01 21:47:42 +0200

    Set thesaurusfunc

--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,30 @@ function M.set_default_options()
 
   cmd.filetype "on"
 
+  vim.cmd [[
+    func Thesaur(findstart, base)
+      if a:findstart
+        return searchpos('\<', 'bnW', line('.'))[1] - 1
+      endif
+      let res = []
+      let h = ''
+      for l in systemlist('aiksaurus ' .. shellescape(a:base))
+        if l[:3] == '=== '
+          let h = '(' .. substitute(l[4:], ' =*$', ')', '')
+        elseif l ==# 'Alphabetically similar known words are: '
+          let h = "\U0001f52e"
+        elseif l[0] =~ '\a' || (h ==# "\U0001f52e" && l[0] ==# "\t")
+          call extend(res, map(split(substitute(l, '^\t', '', ''), ', '), {_, val -> {'word': val, 'menu': h}}))
+        endif
+      endfor
+      return res
+    endfunc
+
+    if exists('+thesaurusfunc')
+      set thesaurusfunc=Thesaur
+    endif
+  ]]
+
   opt.autoindent = true
   opt.completeopt = { "menuone", "preview", "popup" }
   opt.encoding = "utf-8"
