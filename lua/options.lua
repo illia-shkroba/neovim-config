a440b002903462d9942517017f984b311c1b445f
Illia Shkroba <is@pjwstk.edu.pl>
2024-12-08 22:12:48 +0100

    Remove "longest" from completeopt

--- lua/options.lua
+++ lua/options.lua
@@ -35,7 +35,7 @@ function M.set_default_options()
 
   opt.allowrevins = true
   opt.autoindent = true
-  opt.completeopt = { "menuone", "longest", "popup" }
+  opt.completeopt = { "menuone", "popup" }
   opt.encoding = "utf-8"
   opt.expandtab = true
   opt.formatoptions = "tcro/qnl1j"
