7779133d6d232c4669857dbf9e9375ccd02f8b92
Illia Shkroba <is@pjwstk.edu.pl>
2025-01-30 15:00:46 +0100

    Add <C-g> binding for command-line

--- lua/options.lua
+++ lua/options.lua
@@ -633,6 +633,12 @@ function M.set_default_bindings()
     [[s///gc<Left><Left><Left>]],
     { desc = "Populate cmdline with s///gc" }
   )
+  set(
+    "c",
+    [[<C-g>]],
+    [[<Home>cfdo ]],
+    { desc = "Populate cmdline with cfdo at the beginning" }
+  )
 
   -- move
   set(
@@ -818,6 +824,16 @@ function M.set_default_autocommands()
         [[<C-s>]],
         [[s///gc<Left><Left><Left>]],
         { buffer = true, desc = "Populate cmdline with s///gc" }
+      )
+    end,
+  })
+  autocmd("CmdwinEnter", {
+    callback = function()
+      set(
+        { "i" },
+        [[<C-g>]],
+        [[<Home>cfdo ]],
+        { buffer = true, desc = "Populate cmdline with cfdo at the beginning" }
       )
     end,
   })
