70a609ffa11f56d1f1667cd84287a5176117c2f9
Illia Shkroba <is@pjwstk.edu.pl>
2025-03-08 12:03:31 +0100

    Add gs binding to leap to other windows and set `case_sensitive` to `false` in leap

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -4,7 +4,7 @@ return {
     local set = vim.keymap.set
 
     local leap = require "leap"
-    leap.opts.case_sensitive = true
+    leap.opts.case_sensitive = false
     leap.opts.safe_labels = "sfnutrhjklx"
     leap.opts.labels = "abcdefghijklmnopqrstuvwxyz"
 
@@ -13,6 +13,12 @@ return {
       [[<C-s>]],
       "<Plug>(leap)",
       { silent = true, desc = "Leap" }
+    )
+    set(
+      { "n", "v", "o" },
+      [[gs]],
+      "<Plug>(leap-from-window)",
+      { silent = true, desc = "Leap to other windows" }
     )
     set({ "i" }, [[<C-s>]], "<Plug>(leap)", { silent = true, desc = "Leap" })
   end,
