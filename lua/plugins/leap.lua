bee733f3b9bcb8c15e1cfaeefc41c8c1309dc568
Illia Shkroba <is@pjwstk.edu.pl>
2025-03-08 11:51:18 +0100

    Remove unused binding and set safe_labels and labels for leap

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -5,16 +5,12 @@ return {
 
     local leap = require "leap"
     leap.opts.case_sensitive = true
+    leap.opts.safe_labels = "sfnutrhjklx"
+    leap.opts.labels = "abcdefghijklmnopqrstuvwxyz"
 
     set(
       { "n", "v", "o" },
       [[<C-s>]],
-      "<Plug>(leap)",
-      { silent = true, desc = "Leap" }
-    )
-    set(
-      { "n", "v", "o" },
-      "gs",
       "<Plug>(leap)",
       { silent = true, desc = "Leap" }
     )
