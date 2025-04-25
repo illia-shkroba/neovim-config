4a8f54ed95d976363f1bb9bd76230acf68f83ba9
Illia Shkroba <is@pjwstk.edu.pl>
2025-04-25 23:51:36 +0200

    Use `set_repeat_keys` in `leap`

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -4,9 +4,19 @@ return {
     local set = vim.keymap.set
 
     local leap = require "leap"
+    local leap_user = require "leap.user"
+
     leap.opts.case_sensitive = false
     leap.opts.safe_labels = "sfnutrhjklx"
     leap.opts.labels = "abcdefghijklmnopqrstuvwxyz"
+
+    leap_user.set_repeat_keys("<C-n>", "<C-p>", {
+      -- If set to true, the keys will work like the native
+      -- semicolon/comma, i.e., forward/backward is understood in
+      -- relation to the last motion.
+      relative_directions = false,
+      modes = { "n", "x", "o" },
+    })
 
     set(
       { "n", "v", "o" },
