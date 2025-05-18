b0bfcce69b07a2fbb47213ea6a26881c3ef67409
Illia Shkroba <is@pjwstk.edu.pl>
2025-05-18 23:54:49 +0200

    Set options for `mdformat`

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -5,6 +5,6 @@ vim.b.did_markdown_ftplugin = true
 
 local opt_local = vim.opt_local
 
-opt_local.formatprg = "mdformat -"
+opt_local.formatprg = "mdformat --number -"
 opt_local.spell = true
 opt_local.textwidth = 100
