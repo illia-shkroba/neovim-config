070e318e6f01b9bf3d38a54da4219ad7444d4a3a
Illia Shkroba <is@pjwstk.edu.pl>
2025-06-14 11:00:31 +0200

    Use lazy-lock.json

--- lua/package-manager/init.lua
+++ lua/package-manager/init.lua
@@ -5,6 +5,8 @@ vim.cmd [[
 require("package-manager.bootstrap").bootstrap()
 
 return require("lazy").setup("plugins", {
+  root = vim.fn.stdpath "data" .. "/lazy", -- directory where plugins will be installed
+  lockfile = vim.fn.stdpath "config" .. "/lazy-lock.json", -- lockfile generated after running update.
   change_detection = {
     enabled = false,
   },
