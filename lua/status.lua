7bcc961383cd8b430822c127ec7026f6b0964b69
Illia Shkroba <is@pjwstk.edu.pl>
2025-07-09 22:34:47 +0200

    Substitute `ts_status` with `char_search` in `get_statusline`

--- lua/status.lua
+++ lua/status.lua
@@ -1,5 +1,3 @@
-local utils = require "utils"
-
 -- Search status. Returns one of:
 -- * "f" - most recent search was a forward search.
 -- * "b" - most recent search was a backward search.
@@ -7,18 +5,14 @@ local function get_search_status()
   return [[v:searchforward ? 'f' : 'b']]
 end
 
--- Treesitter status. Returns one of:
--- * "f" - either `f` or `t` motion was used.
--- * "b" - either `F` or `T` motion was used.
--- * "-" - none of the `f`, `t`, `F`, `T` motions was used.
-local function get_ts_status()
-  local ts_repeat_move =
-    utils.require_safe "nvim-treesitter.textobjects.repeatable_move"
-  if ts_repeat_move == nil then
-    return nil
-  end
-
-  return [[luaeval("require('nvim-treesitter.textobjects.repeatable_move').last_move == nil") ? '-' : luaeval("require('nvim-treesitter.textobjects.repeatable_move').last_move.opts.forward") ? 'f' : 'b']]
+-- Char search status. Returns one of:
+-- * "F*" - `F` motion was used along with a character denoted as '*'.
+-- * "T*" - `T` motion was used along with a character denoted as '*'.
+-- * "f*" - `f` motion was used along with a character denoted as '*'.
+-- * "t*" - `t` motion was used along with a character denoted as '*'.
+-- * "-" - no char search was performed yet.
+local function get_char_search_status()
+  return [[getcharsearch().char == '' ? '-' : ((getcharsearch().forward ? (getcharsearch().until ? 't' : 'f') : (getcharsearch().until ? 'T' : 'F')) .. getcharsearch().char)]]
 end
 
 -- Current working directory in a concise format.
@@ -27,11 +21,11 @@ local function get_cwd()
 end
 
 local function get_statusline()
-  local ts_status = get_ts_status()
+  local char_search = get_char_search_status()
 
   return "%<%f %h%m%r"
     .. ("|%{" .. get_search_status() .. "}| ")
-    .. (ts_status and ("<%{" .. ts_status .. "}> ") or "")
+    .. (char_search and ("<%{" .. char_search .. "}> ") or "")
     .. ("(%{" .. get_cwd() .. "})")
     .. "%=%-14.(%l,%c%V%) %P"
 end
