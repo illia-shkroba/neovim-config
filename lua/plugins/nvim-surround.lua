14e4737b48c811d0768cf6648733002b65a5d8b3
Illia Shkroba <is@pjwstk.edu.pl>
2025-08-22 21:31:04 +0200

    Add `j` surrounding for Ansible playbooks

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -16,6 +16,13 @@ return {
         change = "cs",
         change_line = "cS",
       },
+      surrounds = {
+        ["j"] = {
+          add = function()
+            return { { [["{{ ]] }, { [[ }}"]] } }
+          end,
+        },
+      },
     }
   end,
 }
