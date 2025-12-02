495a18b8bc804dc0825b69f8f9aae880dab24d74
Illia Shkroba <is@pjwstk.edu.pl>
2025-12-02 21:21:07 +0100

    Use `vim.fs.joinpath`

--- lua/path.lua
+++ lua/path.lua
@@ -7,7 +7,7 @@ function M.step_into(src_dir, dest_dir)
   local n = utils.prefix_length(xs, ys)
 
   local prefix = table.concat(ys, "/", 1, n)
-  return prefix .. "/" .. (ys[n + 1] or "")
+  return vim.fs.joinpath(prefix, ys[n + 1] or "")
 end
 
 function M.extension(path)
