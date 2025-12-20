9111ea3cebcf02b54a6188e10f98a87113492225
Illia Shkroba <is@pjwstk.edu.pl>
2025-12-21 00:09:23 +0100

    Add `match_rg` to `filetypes`

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -29,4 +29,34 @@ end
 M.nvim_to_rg = read(config .. "/etc/filetypes/nvim-to-rg.json")
 M.rg = read(config .. "/etc/filetypes/rg.json")
 
+local function glob(path, pattern)
+  return vim.glob.to_lpeg(pattern):match(path) ~= nil
+end
+
+local function match_rg(nvim_filetype, path)
+  local rg_matches = M.nvim_to_rg[nvim_filetype]
+  if rg_matches == nil then
+    return {}
+  end
+
+  if #rg_matches == 1 or path == nil then
+    return rg_matches
+  end
+
+  return vim.tbl_filter(function(rg_match)
+    for _, pattern in pairs(rg_match.filenames) do
+      if glob(path, pattern) then
+        return true
+      end
+    end
+    return false
+  end, rg_matches)
+end
+
+function M.match_rg(nvim_filetype, path)
+  return vim.tbl_map(function(x)
+    return x.rg_type
+  end, match_rg(nvim_filetype, path))
+end
+
 return M
