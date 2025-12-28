cb7531203d9252469785f84e0c75211154a8e4ca
Illia Shkroba <is@pjwstk.edu.pl>
2025-12-28 18:19:29 +0100

    Add `on_error` option for `marks` provided to `with_marks`

--- lua/mark.lua
+++ lua/mark.lua
@@ -12,7 +12,11 @@ local M = {}
 
 ---@class FrozenMarks
 ---@field buffer_number integer
----@field marks table<integer, {name: string, adjustment?: fun(Mark): Position}>
+---@field marks table<integer, {
+---  name: string,
+---  adjustment?: (fun(Mark): Position),
+---  on_error?: (fun(Mark): Position),
+---}>
 ---@field function_ fun(): nil
 
 ---@param frozen_marks FrozenMarks
@@ -20,6 +24,7 @@ local M = {}
 function M.with_marks(frozen_marks)
   local marks = {}
   local adjustments = {}
+  local on_errors = {}
 
   for _, mark_input in pairs(frozen_marks.marks) do
     local raw_mark =
@@ -32,23 +37,49 @@ function M.with_marks(frozen_marks)
       column = raw_mark[2],
     })
     adjustments[mark_input.name] = mark_input.adjustment
+    on_errors[mark_input.name] = mark_input.on_error
   end
 
   frozen_marks.function_()
 
   for _, mark in pairs(marks) do
+    local function set_mark(position)
+      return pcall(
+        vim.api.nvim_buf_set_mark,
+        frozen_marks.buffer_number,
+        mark.name,
+        position.line,
+        position.column,
+        {}
+      )
+    end
+
     local adjustment = adjustments[mark.name]
       or function(m)
         return { line = m.line, column = m.column }
       end
     local position = adjustment(mark)
-    vim.api.nvim_buf_set_mark(
-      frozen_marks.buffer_number,
-      mark.name,
-      position.line,
-      position.column,
-      {}
-    )
+
+    local success = set_mark(position)
+    if not success then
+      if on_errors[mark.name] ~= nil then
+        local on_error = on_errors[mark.name]
+        set_mark(on_error(mark))
+      else
+        vim.notify(
+          "Caught an error while setting mark '"
+            .. mark.name
+            .. "' in the buffer "
+            .. frozen_marks.buffer_number
+            .. " to line "
+            .. position.line
+            .. " and column "
+            .. position.column
+            .. ". Consider providing `on_error` for the mark.",
+          vim.log.levels.ERROR
+        )
+      end
+    end
   end
 end
 
