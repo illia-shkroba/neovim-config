22334320f2893a6536755192138dc2ba7c0d9534
Illia Shkroba <is@pjwstk.edu.pl>
2026-01-02 22:54:16 +0100

    Set `neoclip` bindings

--- lua/plugins/neoclip.lua
+++ lua/plugins/neoclip.lua
@@ -2,6 +2,14 @@ return {
   "AckslD/nvim-neoclip.lua",
   dependencies = { "ibhagwan/fzf-lua" },
   config = function()
-    require("neoclip").setup()
+    require("neoclip").setup {
+      keys = {
+        fzf = {
+          select = "default",
+          paste = false,
+          paste_behind = false,
+        },
+      },
+    }
   end,
 }
