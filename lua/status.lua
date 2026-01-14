782c94ea2b5f6de420f562038b6dfe6c0c748e93
Illia Shkroba <is@pjwstk.edu.pl>
2026-01-14 21:21:12 +0100

    Remove `get_char_search_status` from `status`

--- lua/status.lua
+++ lua/status.lua
@@ -5,27 +5,14 @@ local function get_search_status()
   return [[v:searchforward ? 'f' : 'b']]
 end
 
--- Char search status. Returns one of:
--- * "F*" - `F` motion was used along with a character denoted as '*'.
--- * "T*" - `T` motion was used along with a character denoted as '*'.
--- * "f*" - `f` motion was used along with a character denoted as '*'.
--- * "t*" - `t` motion was used along with a character denoted as '*'.
--- * "-" - no char search was performed yet.
-local function get_char_search_status()
-  return [[getcharsearch().char == '' ? '-' : ((getcharsearch().forward ? (getcharsearch().until ? 't' : 'f') : (getcharsearch().until ? 'T' : 'F')) .. getcharsearch().char)]]
-end
-
 -- Current working directory in a concise format.
 local function get_cwd()
   return [[substitute(getcwd(0, 0), '^' .. expand('~'), '~', '')]]
 end
 
 local function get_statusline()
-  local char_search = get_char_search_status()
-
   return "%<%f %h%m%r"
     .. ("|%{" .. get_search_status() .. "}| ")
-    .. (char_search and ("<%{" .. char_search .. "}> ") or "")
     .. ("(%{" .. get_cwd() .. "})")
     .. "%=%-14.(%l,%c%V%) %P"
 end
