e79de483db6deb28615c02a7b0a5ace187698a88
Illia Shkroba <is@pjwstk.edu.pl>
2026-01-15 17:17:24 +0100

    Unset `vim.g.no_plugin_maps`
    - It prevented `matchit` plugin bindings setting.

--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -2,9 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter-textobjects",
   branch = "main",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  init = function()
-    vim.g.no_plugin_maps = true
-  end,
   config = function()
     require("nvim-treesitter-textobjects").setup {
       select = {
