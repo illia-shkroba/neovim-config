53b110f189207e9c9bcc1e1702a01d876fe9631f
Illia Shkroba <is@pjwstk.edu.pl>
2026-01-19 19:51:15 +0100

    Set `shiftwidth`, `softtabstop` and `tabstop` for `markdown`

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -5,6 +5,9 @@ vim.b.did_markdown_ftplugin = true
 
 vim.opt_local.formatprg = "mdformat --number -"
 vim.opt_local.spell = true
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.tabstop = 2
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "<filetype>" },
