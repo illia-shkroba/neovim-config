f3ec37604b7ddc18b92880ef7bedfc3551c4ec47
Illia Shkroba <is@pjwstk.edu.pl>
2026-01-31 18:36:50 +0100

    Remove unused function

--- lua/path.lua
+++ lua/path.lua
@@ -20,14 +20,4 @@ function M.step_into(src_dir, dest_dir)
   return vim.fs.joinpath(prefix, ys[n + 1] or "")
 end
 
-function M.extension(path)
-  local chunks = vim.split(vim.fs.basename(path), ".", { plain = true })
-  table.remove(chunks, 1)
-  local extension = table.concat(chunks, ".")
-  if #extension > 0 then
-    extension = "." .. extension
-  end
-  return extension
-end
-
 return M
