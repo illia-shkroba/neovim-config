6fa7cdb133c74d26892a1d0b4f18bff6e149311f
Illia Shkroba <is@pjwstk.edu.pl>
2026-03-24 20:20:32 +0100

    Refactor `scratch` module's functions into a single `scratch` function

--- lua/scratch.lua
+++ lua/scratch.lua
@@ -2,46 +2,25 @@ local M = {}
 
 local utils = require "utils"
 
-function M.retained()
+---@class ScratchInput
+---@field liveness "retained"|"onetime"
+
+---@param scratch_input ScratchInput
+---@return integer
+function M.scratch(scratch_input)
+  local events
+  if scratch_input.liveness == "retained" then
+    events = { "BufWinLeave" }
+  elseif scratch_input.liveness == "onetime" then
+    events = { "BufLeave" }
+  end
+
   local listed = false
   local scratch = true
   local buffer = vim.api.nvim_create_buf(listed, scratch)
   vim.cmd.sbuffer(buffer)
 
-  vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
-    buffer = buffer,
-    callback = function()
-      vim.schedule(function()
-        utils.try(vim.cmd, [[bwipeout! ]] .. buffer)
-      end)
-    end,
-    once = true,
-  })
-  vim.keymap.set({ "n" }, [[ZS]], function()
-    local scratch_lines = vim.api.nvim_buf_get_lines(
-      buffer,
-      0,
-      vim.api.nvim_buf_line_count(buffer),
-      true
-    )
-
-    vim.fn.setreg("s", table.concat(scratch_lines, "\n"))
-    vim.notify([[Changed register "s]], vim.log.levels.INFO)
-  end, {
-    buffer = buffer,
-    desc = [[Paste scratch buffer's text into register "s]],
-  })
-
-  return buffer
-end
-
-function M.onetime()
-  local listed = false
-  local scratch = true
-  local buffer = vim.api.nvim_create_buf(listed, scratch)
-  vim.cmd.sbuffer(buffer)
-
-  vim.api.nvim_create_autocmd({ "BufLeave" }, {
+  vim.api.nvim_create_autocmd(events, {
     buffer = buffer,
     callback = function()
       vim.schedule(function()
