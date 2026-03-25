66137b7f6612ad8e65399f8035bdb7145204e283
Illia Shkroba <is@pjwstk.edu.pl>
2026-03-26 00:33:09 +0100

    Add `normalize` to `text.register`

--- lua/text/register.lua
+++ lua/text/register.lua
@@ -41,4 +41,23 @@ function M.edit(register)
   })
 end
 
+---@param register string
+---@return string
+function M.normalize(register)
+  local r = register:lower()
+  return ({
+    ["!"] = "1",
+    ["@"] = "2",
+    ["#"] = "3",
+    ["$"] = "4",
+    ["%"] = "5",
+    ["^"] = "6",
+    ["&"] = "7",
+    ["*"] = "8",
+    ["("] = "9",
+    [")"] = "0",
+    ["?"] = "/",
+  })[r] or r
+end
+
 return M
