28bad2536d706dacbbe0f54cb54f6b37f658e46a
Illia Shkroba <is@pjwstk.edu.pl>
2026-04-01 00:00:05 +0200

    Move `bind_substitute_origin` to `scratch`

--- init.lua
+++ init.lua
@@ -17,7 +17,6 @@ local register = require "text.register"
 local scratch = require "scratch"
 local scratch_register = require "scratch.register"
 local status = require "status"
-local tracked_region = require "text.tracked_region"
 local utils = require "utils"
 local window_picker = require "window-picker"
 
@@ -372,73 +371,6 @@ local function set_bindings()
     return windows
   end
 
-  ---@class SubstituteOriginInput
-  ---@field binding_buffer_number integer
-  ---@field origin_region Region
-
-  ---@param substitute_origin_input SubstituteOriginInput
-  ---@return nil
-  local function bind_substitute_origin(substitute_origin_input)
-    local tracked =
-      tracked_region.from_region(substitute_origin_input.origin_region)
-
-    ---@param lines table<integer, string>
-    ---@return table<integer, string>
-    local function normalize_substitution(lines)
-      if #lines == 1 and lines[1] == "" then
-        return {}
-      else
-        return lines
-      end
-    end
-
-    ---@return nil
-    local function fix_marks()
-      vim.api.nvim_buf_set_mark(
-        tracked.region.buffer_number,
-        "[",
-        tracked.region.line_begin,
-        tracked.region.column_begin,
-        {}
-      )
-      vim.api.nvim_buf_set_mark(
-        tracked.region.buffer_number,
-        "]",
-        tracked.region.line_end,
-        tracked.region.column_end,
-        {}
-      )
-    end
-
-    vim.keymap.set({ "n" }, [[ZP]], function()
-      local lines = normalize_substitution(
-        vim.api.nvim_buf_get_lines(
-          substitute_origin_input.binding_buffer_number,
-          0,
-          vim.api.nvim_buf_line_count(
-            substitute_origin_input.binding_buffer_number
-          ),
-          true
-        )
-      )
-
-      tracked = tracked_region.substitute(tracked, lines)
-
-      fix_marks()
-    end, {
-      buffer = substitute_origin_input.binding_buffer_number,
-      desc = "Paste scratch buffer's text back to the origin buffer in place of the selected lines by motion",
-    })
-    vim.keymap.set({ "n" }, [[ZE]], function()
-      tracked = tracked_region.substitute(tracked, {})
-
-      fix_marks()
-    end, {
-      buffer = substitute_origin_input.binding_buffer_number,
-      desc = "Delete lines selected by motion in the origin buffer",
-    })
-  end
-
   local function with_change_marks(buffer_number, function_)
     mark.with_marks {
       buffer_number = buffer_number,
@@ -565,7 +497,7 @@ local function set_bindings()
 
           vim.api.nvim_buf_set_lines(buffer, 0, 1, false, region_.lines)
 
-          bind_substitute_origin {
+          scratch.bind_substitute_origin {
             binding_buffer_number = buffer,
             origin_region = region_,
           }
@@ -644,7 +576,7 @@ local function set_bindings()
 
       vim.api.nvim_buf_set_lines(buffer, 0, 1, false, region_.lines)
 
-      bind_substitute_origin {
+      scratch.bind_substitute_origin {
         binding_buffer_number = buffer,
         origin_region = region_,
       }
--- lua/scratch.lua
+++ lua/scratch.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+local tracked_region = require "text.tracked_region"
 local utils = require "utils"
 
 ---@class ScratchInput
@@ -60,6 +61,73 @@ function M.open(scratch_input)
   })
 
   return buffer
+end
+
+---@class SubstituteOriginInput
+---@field binding_buffer_number integer
+---@field origin_region Region
+
+---@param substitute_origin_input SubstituteOriginInput
+---@return nil
+function M.bind_substitute_origin(substitute_origin_input)
+  local tracked =
+    tracked_region.from_region(substitute_origin_input.origin_region)
+
+  ---@param lines table<integer, string>
+  ---@return table<integer, string>
+  local function normalize_substitution(lines)
+    if #lines == 1 and lines[1] == "" then
+      return {}
+    else
+      return lines
+    end
+  end
+
+  ---@return nil
+  local function fix_marks()
+    vim.api.nvim_buf_set_mark(
+      tracked.region.buffer_number,
+      "[",
+      tracked.region.line_begin,
+      tracked.region.column_begin,
+      {}
+    )
+    vim.api.nvim_buf_set_mark(
+      tracked.region.buffer_number,
+      "]",
+      tracked.region.line_end,
+      tracked.region.column_end,
+      {}
+    )
+  end
+
+  vim.keymap.set({ "n" }, [[ZP]], function()
+    local lines = normalize_substitution(
+      vim.api.nvim_buf_get_lines(
+        substitute_origin_input.binding_buffer_number,
+        0,
+        vim.api.nvim_buf_line_count(
+          substitute_origin_input.binding_buffer_number
+        ),
+        true
+      )
+    )
+
+    tracked = tracked_region.substitute(tracked, lines)
+
+    fix_marks()
+  end, {
+    buffer = substitute_origin_input.binding_buffer_number,
+    desc = "Paste scratch buffer's text back to the origin buffer in place of the selected lines by motion",
+  })
+  vim.keymap.set({ "n" }, [[ZE]], function()
+    tracked = tracked_region.substitute(tracked, {})
+
+    fix_marks()
+  end, {
+    buffer = substitute_origin_input.binding_buffer_number,
+    desc = "Delete lines selected by motion in the origin buffer",
+  })
 end
 
 return M
