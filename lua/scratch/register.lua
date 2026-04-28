6350c0099e1f40fca83adefe8f854d39998b33e3
Illia Shkroba <is@pjwstk.edu.pl>
2026-04-28 20:11:51 +0200

    Remove unused `cr` binding

--- lua/scratch/register.lua
+++ lua/scratch/register.lua
@@ -19,14 +19,6 @@ function M.edit(register)
     vim.split(vim.fn.getreg(register), "\n")
   )
 
-  vim.keymap.set("n", [[cr]], function()
-    register = vim.v.register:lower()
-    vim.opt_local.statusline = "@" .. register .. " " .. status.statusline
-    vim.notify([[Switched to register "]] .. register, vim.log.levels.INFO)
-  end, {
-    buffer = buffer,
-    desc = [[Switch to register without changing scratch buffer]],
-  })
   vim.keymap.set("n", [[ZP]], function()
     local scratch_lines = vim.api.nvim_buf_get_lines(
       buffer,
