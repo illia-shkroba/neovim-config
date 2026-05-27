ed4d991a29998a1dbce97194455b2c21e44874ea
Illia Shkroba <is@pjwstk.edu.pl>
2026-05-27 23:00:52 +0200

    Use `hl_op` instead of deprecated `on_yank`

--- init.lua
+++ init.lua
@@ -1685,7 +1685,7 @@ local function set_autocommands()
   })
   vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
-      vim.hl.on_yank { timeout = 300 }
+      vim.hl.hl_op { higroup = "Visual", timeout = 300 }
     end,
   })
 end
