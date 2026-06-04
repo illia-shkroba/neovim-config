3409b803ebfea2205d8a3857baa1b5c608ffae17
Illia Shkroba <is@pjwstk.edu.pl>
2026-06-04 23:59:19 +0200

    Add `type_` and `as_temporary` to `buffer`

--- lua/buffer.lua
+++ lua/buffer.lua
@@ -9,4 +9,26 @@ function M.short_name(buffer)
   return relative or vim.fn.fnamemodify(absolute, ":~")
 end
 
+---@param buffer integer
+---@return "scratch"|"normal"
+function M.type_(buffer)
+  if
+    vim.bo[buffer].buftype == "nofile"
+    and vim.bo[buffer].bufhidden == "hide"
+    and vim.bo[buffer].swapfile == false
+  then
+    return "scratch"
+  else
+    return "normal"
+  end
+end
+
+---@param buffer integer
+---@return nil
+function M.as_temporary(buffer)
+  vim.bo[buffer].buflisted = true
+  vim.bo[buffer].buftype = ""
+  vim.api.nvim_buf_set_name(buffer, vim.fn.tempname())
+end
+
 return M
