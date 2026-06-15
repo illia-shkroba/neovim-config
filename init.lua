d92f0fbf30b9d3c8aae3bbe30cf2524911c85268
Illia Shkroba <is@pjwstk.edu.pl>
2026-06-16 00:00:51 +0200

    Add `Z?` binding

--- init.lua
+++ init.lua
@@ -1195,6 +1195,14 @@ local function set_bindings()
   end, {
     desc = "Paste buffer's text into register",
   })
+  vim.keymap.set("n", [[Z?]], function()
+    local last_accessed_window = vim.fn.winnr "#"
+    return last_accessed_window > 0 and [["/ZX<C-w>p<C-w>m]] or [["/ZXZQ]]
+  end, {
+    expr = true,
+    remap = true,
+    desc = [[Paste buffer's text into register "/, and close window]],
+  })
   vim.keymap.set("n", [[ZG]], function()
     local last_accessed_window = vim.fn.winnr "#"
     return last_accessed_window > 0 and [["gZX<C-w>p<C-w>m]] or [["gZXZQ]]
